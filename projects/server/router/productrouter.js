4afcafa811706ace97793c3603b15f75a6ad8ed4
ahmadm77 <ahmadmawardi007@gmail.com>
2023-08-03 14:53:55 +0700

    add : usercontroller,middleware,product,router

--- projects/server/router/productrouter.js
+++ projects/server/router/productrouter.js
@@ -1,13 +1,13 @@
 
 const {productController} = require('../controller')
-const { verifyToken } = require('../midleware/auth')
+const { verifyToken } = require('../middleware/auth')
 const router = require('express').Router()
 
 const { multerUpload } = require('../midleware/multer')
 
 
 router.get('/',productController.getProduct)
-router.post('/addpro',   multerUpload('./assets/productImg', 'product').single('file'), productController.addProduct)
+router.post('/addpro',verifyToken,multerUpload('./assets/productImg', 'product').single('file'), productController.addProduct)
 
 
 module.exports = router
